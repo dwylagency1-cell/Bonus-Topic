2e5e39e90073f36509d8d019fc7fdf16409db722
Dekiru <dwyl.agency1@gmail.com>
2026-07-01 15:04:55 +0530

    Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar(props) {
+    return <div>
+        <div>
+            <h3>{props.theme}</h3>
+            <button>Change theme</button>
+        </div>
+    </div>
+}
+export default Navbar
