296199380cb105f59a6e56314866afdb7e4ccbf6
Dekiru <dwyl.agency1@gmail.com>
2026-07-02 13:07:51 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Navbar from './components/Navbar.jsx'
 
 
 function App() {
-  const [theme, setTheme] = useState('dark')
+  const [theme, setTheme] = useState('light')
   return <div>
     <h1>Theme is {theme}</h1>
-    <Navbar theme= {theme}></Navbar>
+    <Navbar theme= {theme} setTheme = {setTheme}></Navbar>
 
   </div>
 }
